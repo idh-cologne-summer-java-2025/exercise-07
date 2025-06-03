e66b1a4c417e14196a956e08ea4714574ad7761f
Jürgen Hermes <hermesj@uni-koeln.de>
2025-06-03 16:25:08 +0200

    Initial commit

---
+++ src/idh/java/searchtrees/BITApplication.java
@@ -0,0 +1,33 @@
+package idh.java.searchtrees;
+
+public class BITApplication {
+
+	public static void main(String[] args) {
+
+		// Wie gehabt Binärbaum anlegen und füllen
+		BinaryIntegerTree bit = new BinaryIntegerTree();
+		bit.addValue(5);
+		bit.addValue(7);
+		bit.addValue(9);
+		bit.addValue(6);
+		bit.addValue(2);
+		bit.addValue(1);
+
+		bit.printInOrder();
+		
+		System.out.println(bit.contains(7)); // sollte true sein
+		
+		// Löschen eines Knotens
+		bit.delete(7);
+		
+		System.out.println(bit.contains(7)); // sollte nun false sein
+		System.out.println(bit.contains(9)); // sollte noch true sein
+		
+		// Jetzt Probe auf's Exempel: Löschen der Wurzel
+		bit.delete(5);
+		
+		System.out.println(bit.contains(5)); // sollte nun false sein
+		System.out.println(bit.contains(9)); // sollte noch true sein
+
+	}
+}
---
+++ src/idh/java/searchtrees/BinaryIntegerTree.java
@@ -0,0 +1,120 @@
+package idh.java.searchtrees;
+
+/**
+ * Klasse zum Speichern von int-Werten in einem sortierten Binärbaum
+ */
+public class BinaryIntegerTree {
+
+	/**
+	 * Innere Klasse, die einen int-Binärbaumknoten repräsentiert
+	 */
+	class BinaryIntegerTreeNode {
+
+		public BinaryIntegerTreeNode(int value) {
+			this.value = value;
+		}
+
+		BinaryIntegerTreeNode left;
+		BinaryIntegerTreeNode right;
+		int value;
+
+		/**
+		 * Rekursive Methode für das Hinzufügen von int-Werten
+		 * 
+		 * @param value int-Wert
+		 * @return true, wenn Wert hinzugefügt wurde, false, wenn er schon vorhandem war
+		 */
+		public boolean addValue(int newValue) {
+            if (newValue == value) {
+                return false; // Wert bereits vorhanden
+            } else if (newValue < value) {
+                if (left == null) {
+                    left = new BinaryIntegerTreeNode(newValue);
+                    return true;
+                } else {
+                    return left.addValue(newValue);
+                }
+            } else { // newValue > value
+                if (right == null) {
+                    right = new BinaryIntegerTreeNode(newValue);
+                    return true;
+                } else {
+                    return right.addValue(newValue);
+                }
+            }
+        }
+
+        public void printInOrder() {
+            if (left != null) {
+                left.printInOrder();
+            }
+            System.out.print(value + " ");
+            if (right != null) {
+                right.printInOrder();
+            }
+        }
+
+		public boolean contains(int searchValue) {
+            if (searchValue == value) {
+                return true;
+            } else if (searchValue < value) {
+                return (left != null) && left.contains(searchValue);
+            } else {
+                return (right != null) && right.contains(searchValue);
+            }
+        }
+		
+		public boolean delete(int value) {
+			//TODO: Implement
+			return false;
+		}
+	}
+
+	/**
+	 * Die Wurzel des Baums
+	 */
+	private BinaryIntegerTreeNode root;
+
+	/**
+	 * Methode für das Hinzufügen von int-Werten in den Baum
+	 * 
+	 * @param value int-Wert
+	 * @return true, wenn Wert hinzugefügt wurde, false, wenn er schon vorhandem war
+	 */
+	public boolean addValue(int value) {
+        if (root == null) {
+            root = new BinaryIntegerTreeNode(value);
+            return true;
+        } else {
+            return root.addValue(value);
+        }
+    }
+
+	/**
+	 * Soll den Baum in der sortierten Reihenfolge ausgeben
+	 */
+	public void printInOrder() {
+        if (root != null) {
+            root.printInOrder();
+        } else {
+            System.out.println("Der Baum ist leer.");
+        }
+    }
+
+	/**
+	 * Überprüft, ob der spezifizierte Wert enthalten ist.
+	 */
+	public boolean contains(int value) {
+        return root != null && root.contains(value);
+    }
+	
+	
+	/**
+	 * Löscht den übergebenen Wert aus dem Baum.
+	 */
+	public boolean delete(int value) {
+		//TODO: Implement
+		return false;
+	}
+
+}
