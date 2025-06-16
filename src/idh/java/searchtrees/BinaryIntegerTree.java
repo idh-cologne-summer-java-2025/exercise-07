cd8674dea57fae2687668917642fe423650a76be
Yuima <Yuima@DwieDosenlaptop>
2025-06-16 22:27:40 +0200

    *crrrrk*
    Husten! Wir sind grandios an der definitiven Bestimmung gescheitert, wie
    man letztendlich feststellen soll, welchen Node man entlanglaufen soll.
    Der Rest der Theorie für Fall 1 und 2 sollte allerdings im Code
    vorhanden sein. OVER! *crrrrrrrk*

--- src/idh/java/searchtrees/BinaryIntegerTree.java
+++ src/idh/java/searchtrees/BinaryIntegerTree.java
@@ -65,7 +65,60 @@ public class BinaryIntegerTree {
         }
 		
 		public boolean delete(int value) {
-			//TODO: Implement
+			
+			/**
+			 * Ich hab hier den Delete-Abschnitt für den linken Suchbaum und für den rechten Suchbaum
+			 * geschrieben, da man ja den current-Value nicht mit ner Null-Deklaration löschen kann.
+			 * Deshalb muss glaube ich der Baum in links und rechts aufgeteilt werden, um danut arbeiten zu können.
+			 * */
+			if (left.value>=value) {
+				if(left.left==null&&left.right==null) {
+					left=null; 
+					return true;
+					/**Ich bin mir grad noch nicht ganz sicher,
+					ob man damit bis in die hinterste Ecke des Baumes kommt.
+					Aber mit this.value kommt man ja nicht weiter, da value in primitiver Datentyp ist*/
+				}
+				/**hiermit wird Case 2 abgedeckt*/
+				else if(left.left==null&&left.right!=null&&left.value==value) {
+					left=left.right;
+					return true;
+				}
+				else if(left.left!=null&&left.right==null&&left.value==value) {
+					left=left.left;
+					return true;
+				}
+				//Kommen wir nun zu Case 3:...
+				else if(left.left!=null&&left.right!=null&&left.value==value){
+					
+				}
+				//Wenn wir auf dieser Ebene nicht auf unseren gesuchten Value gestoßen sind,
+				//diggen wir eine Ebene tiefer in das Konstrukt.
+				left.delete(value);
+				
+			}
+			//UND NUN DEN GAAAAAANZEN SPAß AUCH FÜR DIE ANDERE SEITE, YEHAAAAAW
+			else if (right.value<=value) {
+				if(right.left==null&&right.right==null&&right.value==value) {
+					right=null;
+					return true;
+					//Same gilt wie bei dieser Bedingung vom linken Zweig
+				}
+				else if(right.left==null&&right.right!=null&&right.value==value) {
+					right=right.right;
+					return true;
+				}
+				else if(right.left!=null&&right.right==null&&right.value==value) {
+					right=right.left;
+					return true;
+				}
+				else if(right.left!=null&&right.right!=null&&right.value==value) {
+					
+				}
+				right.delete(value);
+			}
+			
+			
 			return false;
 		}
 	}
@@ -113,7 +166,17 @@ public class BinaryIntegerTree {
 	 * Löscht den übergebenen Wert aus dem Baum.
 	 */
 	public boolean delete(int value) {
-		//TODO: Implement
+		if (root.value==value) {
+			if (root.left==null && root.right==null) {
+				root = null;
+				return true;
+			} else {
+				root.delete(value);
+			}
+		}
+		if (contains(value)==true) {
+			root.delete(value);
+		}
 		return false;
 	}
 
